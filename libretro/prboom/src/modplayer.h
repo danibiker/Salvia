bbc65148053950db791b67fea84e47ee7addf801
danibiker <153503918+danibiker@users.noreply.github.com>
2026-08-20 09:10:32 +0200

    Deleted prboom to recreate it

--- libretro/prboom/src/modplayer.h
+++
@@ -1,5 +0,0 @@
-#ifndef MODPLAYER_H
-#define MODPLAYER_H
-#include "musicplayer.h"
-extern const music_player_t mod_player;
-#endif
--- libretro/prboom/src/oggplayer.h
+++
@@ -1,8 +0,0 @@
-#ifndef OGGPLAYER_H
-#define OGGPLAYER_H
-
-#include "musicplayer.h"
-
-extern const music_player_t ogg_player;
-
-#endif
